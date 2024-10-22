b1dd3d14bae8861d133abf3f0b4c87c43dab61ae
Steve Anderson <stevenix@protonmail.com>
2024-10-22 15:56:45 -0300

    adding astrovim stuff

---
+++ dot_config/nvim/lua/community.lua
@@ -0,0 +1,15 @@
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
+  --{ import = "astrocommunity.motion.leap-nvim" },
+  { import = "astrocommunity.motion.hop-nvim" },
+  -- import/override with your plugins folder
+}
